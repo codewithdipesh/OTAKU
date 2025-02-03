a31f5d90c9cf84c74b36b3b6394ae98846859e5a
Dipesh Pal <codewithdipesh2022@gmail.com>
2025-02-03 20:16:19 +0530

    initiated mangaReader

---
+++ app/src/main/java/com/codewithdipesh/mangareader/ui/theme/Type.kt
@@ -0,0 +1,45 @@
+package com.codewithdipesh.mangareader.ui.theme
+import com.codewithdipesh.mangareader.R
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+
+val regular = FontFamily(
+    Font(R.font.regular, FontWeight.Normal)
+)
+
+val japanese = FontFamily(
+    Font(R.font.japanese_style_font, FontWeight.Normal)
+)
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+)
