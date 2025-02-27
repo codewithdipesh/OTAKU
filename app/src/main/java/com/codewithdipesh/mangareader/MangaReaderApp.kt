3c913d9745757ff050c8a986c34469167ba40922
Dipesh Pal <codewithdipesh2022@gmail.com>
2025-02-27 20:28:50 +0530

    clearing coil cache after closing the app

--- app/src/main/java/com/codewithdipesh/mangareader/MangaReaderApp.kt
+++ app/src/main/java/com/codewithdipesh/mangareader/MangaReaderApp.kt
@@ -1,8 +1,21 @@
 package com.codewithdipesh.mangareader
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.util.CoilUtils
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MangaReaderApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    //clearing the coil cache after closing the app
+    override fun onTerminate() {
+        super.onTerminate()
+        ImageLoader(this).memoryCache?.clear()
+        ImageLoader(this).diskCache?.clear()
+    }
 }
